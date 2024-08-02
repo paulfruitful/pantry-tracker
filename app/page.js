2237d4b155ad27543819fe7f0ee48bc3a35daec0
Paul Fruitful <fruitful2007@outlook.com>
2024-08-02 21:37:25 +0100

    Landing page work

--- app/page.js
+++ app/page.js
@@ -6,6 +6,16 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <></>
+    <>
+    <div className='dark:bg-[#1A1A1A] grid' >
+
+      <div className='flex w-full'>
+        <span className='juistify-self-start p-6 text-3xl dark:text-white'>Trackr</span>
+      </div>
+
+
+    </div>
+    
+    </>
   )
 }
