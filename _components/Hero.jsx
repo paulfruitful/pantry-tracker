820f16fedd216b94fe1e488b877846ce3bc89123
Paul Fruitful <fruitful2007@outlook.com>
2024-08-03 20:20:56 +0100

    fixures

--- _components/Hero.jsx
+++ _components/Hero.jsx
@@ -1,10 +1,10 @@
 const Hero = () => {
   return (
-    <div className="flex justify-between px-3 lg:px-8 ">
+    <div className="flex justify-between px- lg:px-8 w-full ">
 
         <span className="text-lg border-b-2 lg:p-0 pl-2  border-solid  ">Inventory <span className="font-bold  text-xl">(0)</span></span>
 
-        <button className=" p-3 bg-green-600 hover:p-6 text-white rounded-[10px] shadow-sm">+ Add Pantry</button>
+        <button className=" p-3 bg-green-600  text-white rounded-[10px] shadow-sm">+ Add Pantry</button>
      
     </div>
   )
